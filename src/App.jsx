3d61cbaae91c57512fef8c9e1f29181ba2a2675b
aymiwash <emi.luong@gmail.com>
2023-07-25 16:19:29 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,55 @@ import uuid from "react-uuid";
 
 function App() {
 
-  contactsJSON.forEach((contact)=>{
+  contactsJSON.forEach((contact) => {
     contact.id = uuid()
   })
   const [contacts, setContacts] = useState(contactsJSON.slice(0, 5))
 
-  const addRandomContact = ()=>{
+  const addRandomContact = () => {
     const randomContactIndex = Math.floor(Math.random() * contactsJSON.length) + 5
     const randomContact = contactsJSON[randomContactIndex]
-    if(randomContact !== undefined){
+    if (randomContact !== undefined) {
       setContacts(previousContacts =>
         [...previousContacts, randomContact]
       )
     }
   }
 
-  const sortByName = ()=>{
-    const sortedContactsByName = [...contacts].sort((a, b)=>{
-      return a.name.localeCompare(b.name)
+  const sortByName = () => {
+    setContacts(previousContacts=>{
+      const sortedContactsByName = [...previousContacts].sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      })
+      return sortedContactsByName
     })
-    setContacts(sortedContactsByName)
   }
 
-  const sortByPopularity = ()=>{
-    const sortedContactsByPopularity = [...contacts].sort((a, b)=>{
-      return a.popularity - b.popularity
+  const sortByPopularity = () => {
+    setContacts(previousContacts => {
+      const sortedContactsByPopularity = [...previousContacts].sort((a, b) => {
+        return a.popularity - b.popularity
+      })
+      return sortedContactsByPopularity
     })
-    setContacts(sortedContactsByPopularity)
   }
 
-  const deleteContact = (id)=>{
+  const deleteContact = (id) => {
     const contactId = contacts.find(contact => contact.id === id)
     const contactIndex = contacts.indexOf(contactId)
-    setContacts(previousContacts=>{
+    setContacts(previousContacts => {
       const newContactsArray = [...previousContacts]
       newContactsArray.splice(contactIndex, 1)
-    return newContactsArray} )
+      return newContactsArray
+    })
   }
 
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
-
       <button className="button" onClick={addRandomContact}>Add Random Contact</button>
       <button className="button" onClick={sortByName}>Sort by name</button>
       <button className="button" onClick={sortByPopularity}>Sort by popularity</button>
-
       <table className="contacts-table">
         <thead>
           <tr>
@@ -63,20 +66,20 @@ function App() {
           </tr>
         </thead>
         <tbody>
-        {
-          contacts.map(oneContact =>{
-            return (
-              <tr key={oneContact.id}>
-                <td><img className="contact-image" src={oneContact.pictureUrl ? oneContact.pictureUrl : ""} alt={oneContact.name}/></td>
-                <td><h3>{oneContact.name}</h3></td>
-                <td>{oneContact.popularity.toFixed(2)}</td>
-                <td className="oscar">{oneContact.wonOscar ? "🏆" : null}</td>
-                <td className="oscar">{oneContact.wonEmmy ? "🏆" : null}</td>
-                <td><button className="button delete" onClick={()=>{deleteContact(oneContact.id)}}>Delete</button></td>
-              </tr>
-            )
-          })
-        }
+          {
+            contacts.map(oneContact => {
+              return (
+                <tr key={oneContact.id}>
+                  <td><img className="contact-image" src={oneContact.pictureUrl ? oneContact.pictureUrl : ""} alt={oneContact.name} /></td>
+                  <td><h3>{oneContact.name}</h3></td>
+                  <td>{oneContact.popularity.toFixed(2)}</td>
+                  <td className="oscar">{oneContact.wonOscar ? "🏆" : null}</td>
+                  <td className="oscar">{oneContact.wonEmmy ? "🏆" : null}</td>
+                  <td><button className="button delete" onClick={() => { deleteContact(oneContact.id) }}>Delete</button></td>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </table>
     </div>
